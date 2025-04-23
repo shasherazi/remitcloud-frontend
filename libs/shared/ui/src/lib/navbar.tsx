4f686637b4f54c15d7fc977abe11198388f039d8
shasherazi <hassanrandomz@gmail.com>
2025-04-23 18:34:58 +0500

    Basic navbar

---
+++ libs/shared/ui/src/lib/navbar.tsx
@@ -0,0 +1,12 @@
+export function Navbar() {
+  return (
+    <div className="min-h-screen min-w-[280px] border-1 border-r-[#EAECF0] py-8 px-6">
+      <h1 className="text-3xl font-bold">
+        {/* TODO: change font acc to figma and add logo */}
+        RemitUnion
+      </h1>
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ todo.md
@@ -0,0 +1,7 @@
+# TODOs
+
+Search for "todo" in the project to find all TODOs.
+
+# Design
+
+- Get the correct fonts
